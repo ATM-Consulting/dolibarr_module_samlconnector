508e31040e55130e09111d89189fee7d40cdb919
Quentin VIAL--GOUTEYRON <quentin.vial-gouteyron@atm-consulting.fr>
2025-08-12 08:32:55 +0200

    FIX : Multicompany Management : Always redirect if transverse mode enabled

--- acs.php
+++ acs.php
@@ -70,28 +70,25 @@ if($login->isAuthenticated()) {
         $_SESSION['dol_company'] = $conf->global->MAIN_INFO_SOCIETE_NOM;
         $_SESSION['dol_samlconnector_fk_idp'] = $fk_idp;
 
-        if(GETPOSTISSET('entity')) {
-            $_SESSION['dol_entity'] = GETPOST('entity','int');
-        } else {
-            $entitytoconnect = $user->entity;
-            if (!empty(getDolGlobalInt('MULTICOMPANY_TRANSVERSE_MODE'))) {
-                $sql = "SELECT uu.entity";
-                $sql.= " FROM " . $db->prefix() . "usergroup_user as uu";
-                $sql.= ", " . $db->prefix() . "entity as e";
-                $sql.= " WHERE uu.entity = e.rowid AND e.visible < 2"; // Remove template of entity
-                $sql.= " AND uu.fk_user = " . $user->rowid;
+        $entityToConnect = GETPOSTISSET('entity') ? GETPOST('entity', 'int') : $user->entity;
+        //Extract if transverse mode enabled we redirect on right entity
+        if (!empty(getDolGlobalInt('MULTICOMPANY_TRANSVERSE_MODE'))) {
+            $sql = "SELECT uu.entity";
+            $sql.= " FROM " . $db->prefix() . "usergroup_user as uu";
+            $sql.= ", " . $db->prefix() . "entity as e";
+            $sql.= " WHERE uu.entity = e.rowid AND e.visible < 2"; // Remove template of entity
+            $sql.= " AND uu.fk_user = " . $user->rowid;
 
-                dol_syslog("functions_mc::check_user_password_mc sql=" . $sql, LOG_DEBUG);
-                $result = $db->query($sql);
-                if (!empty($result)) {
-                    while($array = $db->fetch_array($result)) { // user allowed if at least in one group
-                        $entitytoconnect = $array['entity'];
-                        break; // stop in first entity
-                    }
+            dol_syslog("functions_mc::check_user_password_mc sql=" . $sql, LOG_DEBUG);
+            $result = $db->query($sql);
+            if (!empty($result)) {
+                while($array = $db->fetch_array($result)) { // user allowed if at least in one group
+                    $entityToConnect = $array['entity'];
+                    break; // stop in first entity
                 }
             }
-            $_SESSION['dol_entity'] = $entitytoconnect;
         }
+        $_SESSION['dol_entity'] = $entityToConnect;
 
         // Store value into session (values stored only if defined)
         if(! empty($dol_hide_topmenu)) $_SESSION['dol_hide_topmenu'] = $dol_hide_topmenu;
